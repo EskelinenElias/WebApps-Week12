09f16c1b9e4268bd6b530d2c76a49571df22673e
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-02-10 18:08:14 +0200

    Small fix

--- server/src/models/Book.ts
+++ server/src/models/Book.ts
@@ -12,6 +12,6 @@ const BookSchema = new Schema<IBook>({
   pages: { type: Number, required: true }
 });
 
-const Book = model<IBook>("bkkooks", BookSchema);
+const Book = model<IBook>("books", BookSchema);
 
 export { Book, IBook };
