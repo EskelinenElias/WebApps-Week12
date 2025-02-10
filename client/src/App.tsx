7cf971d0382866efb5506d0b076c2adcc9e34ed2
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-02-10 16:51:25 +0200

    Added back the <h1> element to the client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <h1>Books</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<BookForm/>}/>
